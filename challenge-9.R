a0a83ec1713bb99f19168f1bda757277c10a4438
Philipp Kessling <philipp.kessling@haw-hamburg.de>
2021-05-04 10:08:21 +0200

    add challenge

---
+++ challenge-9.R
@@ -0,0 +1,15 @@
+# Challenge 9
+# P. Kessling, 29.04.2021
+
+# Heute wollen wir das Kommunikationsverhalten von Twitter-User*innen zu einem
+# bestimmtem Thema untersuchen. Suche dafür ein aktuelles Thema und nutze die Twitter-API.
+# Durchsuche Twitter nach dem Thema, beachte dabei, dass wir zu mindestens 1500 Tweet
+# brauchen.
+# Für die einhundert aktivsten Nutzer bestimmen wir das Kommunikations verhalten genauer.
+# Lade für diese Nutzer jeweils die letzten 1000 Tweets, falls so viele vorhanden sind
+# und Werte visuell nach der Zeit aus.
+# ACHTUNG: das Laden der Daten kann eine ganze Weile in Anspruch nehmen.
+# * Erstelle eine Visualisierung des gesamten Kommunikatinosaufkommen, aufgelöst in 5 Minuten-Intervallen.
+# * Erstelle eine Visualisierung der aktivsten Nutzer.
+# * Erstelle eine Visualisierung der von diesen Nutzern am häufigsten genutzen Hashtags.
+
